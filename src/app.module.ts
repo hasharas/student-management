d28f5d574730a8a8dd19ae1a10df5f091d472267
hasharas <hasharamsankalpam@gmail.com>
2024-08-19 03:14:43 +0530

    add first data post metherd using and test it postmen using

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { DbModule } from './student/student.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 
 @Module({
   imports: [DbModule,ConfigModule.forRoot({ isGlobal:true}), UserModule],
-  controllers: [],
-  providers: [],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
